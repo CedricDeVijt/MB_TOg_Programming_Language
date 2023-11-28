f000b44348143bf79b8a4abe335fca1bc17cc880
ArneDePeuter <arne@depeuter.org>
2023-11-28 16:54:23 +0100

    base

--- src/datatypes/Token.cpp
+++ src/datatypes/Token.cpp
@@ -1,5 +1,1 @@
-//
-// Created by Arne De Peuter on 23/11/2023.
-//
-
 #include "Token.h"
--- src/datatypes/Token.h
+++ src/datatypes/Token.h
@@ -2,9 +2,34 @@
 #define MB_TOG_PROGRAMMING_LANGUAGE_TOKEN_H
 
 #include <list>
+#include <string>
+
+enum class TokenType {
+    Identifier,
+    Var,
+    If,
+    Else,
+    Function,
+    While,
+    OpenParen,
+    CloseParen,
+    Equals,
+    Plus,
+    Minus,
+    Divide,
+    Times,
+    EOF
+};
 
 class Token {
+public:
+    Token(const std::string value = "", TokenType tokenType, int line, int column) = default;
 
+private:
+    std::string value;
+    TokenType tokenType;
+    int line;
+    int column;
 };
 
 typedef std::list<Token> Tokens;
