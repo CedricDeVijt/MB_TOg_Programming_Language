2be153e6f807fcc372f6d87495e1c3ee9f5f1b73
camilledevuyst <devuyst.camille@gmail.com>
2023-11-30 14:30:21 +0100

    lexer gedaan

--- src/modules/Lexer.cpp
+++ src/modules/Lexer.cpp
@@ -1,5 +1,36 @@
 #include "Lexer.h"
 
 Tokens Lexer::lex(const std::string &input) {
-    return Tokens();
+
+    Tokens tokenList;
+    size_t pos = 0;
+
+
+    std::regex var_regex("var");
+    std::regex identifier_regex("[a-zA-Z_][a-zA-Z0-9_]*");
+    std::regex equals_regex("=");
+    std::regex integer_regex("\\d+");
+
+
+    while (pos < input.size()) {
+        std::smatch match;
+
+        if (std::regex_search(input.begin() + pos, input.end(), match, var_regex)) {
+            tokenList.push_back({TokenType::VAR,0,0, match[0]});
+        } else if (std::regex_search(input.begin() + pos, input.end(), match, identifier_regex)) {
+            tokenList.push_back({TokenType::IDENTIFIER,0,0, match[0]});
+        } else if (std::regex_search(input.begin() + pos, input.end(), match, equals_regex)) {
+            tokenList.push_back({TokenType::EQUALS,0,0, match[0]});
+        } else if (std::regex_search(input.begin() + pos, input.end(), match, integer_regex)) {
+            tokenList.push_back({TokenType::INTEGER,0,0, match[0]});
+        } else {
+            std::cerr << "Invalid token at position " << pos << ": " << input[pos] << std::endl;
+            exit(1);
+        }
+
+        pos += match.position() + match.length();
+    }
+
+
+    return tokenList;
 }
--- src/modules/Lexer.h
+++ src/modules/Lexer.h
@@ -1,10 +1,12 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <iostream>
 #include "../datatypes/Token.h"
+#include <regex>
+using namespace std;
 
-class Lexer
-{
+class Lexer{
 public:
     static Tokens lex(const std::string &input);
 };
