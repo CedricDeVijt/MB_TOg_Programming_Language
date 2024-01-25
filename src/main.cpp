5a2c6613f1a51c0f27491cec0c22b60d1144b66b
CedricDeVijt <cedric.devijt@gmail.com>
2024-01-25 19:58:29 +0100

    commented main

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,35 @@
 
 int main(int argc, char *argv[]) {
 
-    std::string filename = "../../data/input/Rekenmachine.txt";
-    /*if (argc!=2) {
-        std::cout << "Didn't get correct run arguments...\n Input a filename to interpret: ";
-        std::cin >> filename; // example: "../../data/input/Rekenmachine.txt"
-    } else {
-        filename = argv[1];
-    }*/
-
-    std::fstream file(filename);
-    if (!file.is_open()) {
-        throw FileNotFoundError(filename);
-    }
-
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-
-    CFG cfg("../../src/cfg.json");
-    MyLang mylang(cfg);
-
-    mylang.interpret(buffer.str());
-
-    /*std::string input = "var x = (5 + 2)";
-    Tokens tokens = Lexer::lex(input);
-    for(const Token& t: tokens){
-        std::cout << t << std::endl;
-
-    }*/
+//    std::string filename = "../../data/input/Rekenmachine.txt";
+//    /*if (argc!=2) {
+//        std::cout << "Didn't get correct run arguments...\n Input a filename to interpret: ";
+//        std::cin >> filename; // example: "../../data/input/Rekenmachine.txt"
+//    } else {
+//        filename = argv[1];
+//    }*/
+//
+//    std::fstream file(filename);
+//    if (!file.is_open()) {
+//        throw FileNotFoundError(filename);
+//    }
+//     */
+//
+//    std::stringstream buffer;
+//    buffer << file.rdbuf();
+//
+//    CFG cfg("../../src/cfg.json");
+//    MyLang mylang(cfg);
+//
+//    mylang.interpret(buffer.str());
+//
+//    /*
+//    /*std::string input = "var x = (5 + 2)";
+//    Tokens tokens = Lexer::lex(input);
+//    for(const Token& t: tokens){
+//        std::cout << t << std::endl;
+//
+//    }*/
 
 
     return 0;
